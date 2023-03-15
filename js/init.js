0e4c06b4106fbf02e1a875a1c520ad1e8f649386
ejemforma43 <ejemforma43@gmail.com>
2023-03-15 12:36:10 -0600

    Cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDcfZjmN5AO43wYS-eyI3sAf8bA-PLwNm8",
+  authDomain: "rickchat-e33b1.firebaseapp.com",
+  projectId: "rickchat-e33b1",
+  storageBucket: "rickchat-e33b1.appspot.com",
+  messagingSenderId: "613023494404",
+  appId: "1:613023494404:web:0301e8e0d54f876a562051",
+  measurementId: "G-P208VHX7NN"
+    });
